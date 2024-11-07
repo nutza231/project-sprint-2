b2f9a6ee518cafad01ff8fd062975427abb0dd84
nutza <www.nutzazaza@gmail.com>
2024-11-07 12:14:01 +0700

    first navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 
 
 
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  return (<>
+    <Navbar />
+    
+    </>
+    
+
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (    
+        <div className=' '>
+        <div className='flex justify-end pt-0 px-0'> 
+            <span>
+                Sign in / 
+            </span>
+            <span>
+                Sign up
+            </span>
+        </div>
+            <div className="flex justify-between items-center w-full bg-slate-600 text-white px-4 py-2 ">
+                <div className="text-xl font-bold px-2">Milky Tea-Rex</div>
+                
+                <div className="flex flex-row items-center space-x-4">
+                    <div className="rounded-md bg-red-600 px-4 py-1 text-sm">
+                        ชาของฉัน
+                    </div>
+
+                    <div className="flex items-center space-x-4">
+                        <span>Home</span>
+                        <span>About</span>
+                        <span>Menu</span>
+                        <span>Product</span>
+                        <span className="flex items-center space-x-3">
+                            <span>Cart</span>
+                            <img
+                                src="https://tse2.mm.bing.net/th?id=OIP.36W6g6n6PVVHU4tJDXm43AHaHK&pid=Api&P=0&h=220"
+                                className="w-6 h-6"
+                                alt="cart icon"
+                            />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar
