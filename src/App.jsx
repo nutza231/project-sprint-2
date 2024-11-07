b75db20c9ef86faec686e782dfa3522f030b413b
Teerathep-Ng <Teerathep.Ng@gmail.com>
2024-11-07 13:52:20 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
 
 
 
 
 
 export default function App() {
-  return (<>
-    <Navbar />
-    
-    </>
-    
-
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
